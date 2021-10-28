4f2394d5c31870e50a39aa9e628708d2e78308c1
warhammer327 <rizwansm@outlook.com>
2021-10-29 00:03:33 +0600

    template vim

---
+++ .vim/templates/skeleton.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define debug(x) printf("-----------------%d\n",x);
+#define LEN sizeof(a)/sizeof(a[0])
+#define MEM(arr,k) memset(arr,k,sizeof arr)
+#define input freopen("01input.txt","r",stdin)
+#define output freopen("01output.txt","w",stdout)
+#define go ios::sync_with_stdio(false);cin.tie(NULL)
+ 
+using namespace std;
+ 
+int X8[] = {0, 1, 0, -1, 1, -1, 1, -1};
+int Y8[] = {1, 0, -1, 0, 1, -1, -1, 1};
+int X4[] = {1, -1, 0, 0};
+int Y4[] = {0, 0 , 1, -1};
+
+int main(){
+
+    return 0;
+}
