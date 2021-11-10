d85d04c9788c53fc8c7bdf4e9479bec6612c69d8
warhammer327 <rizwansm@outlook.com>
2021-11-11 00:01:44 +0600

    swapped minus with underscore

--- start_script.sh
+++ start_script.sh
@@ -4,6 +4,10 @@ xrandr --output eDP-1 --right-of HDMI-1
 xrandr --output eDP-1 --brightness 0.85
 feh --bg-scale ~/Pictures/debian_wallpaper.jpg
 setxkbmap -option caps:escape
+#swap underscore with minus
+xmodmap -e "keycode 20 = underscore minus underscore minus"
+#to get back to normal
+#sudo xmodmap -e "keycode 20 = minus underscore minus underscore"
 picom &
 #guake might not be installed 
 guake &
