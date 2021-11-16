fe033b0773604b62bfa1b9459dc82e53236f1422
warhammer327 <rizwansm@outlook.com>
2021-11-16 20:30:38 +0600

    probably fixed screen tearing

--- start_script.sh
+++ start_script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-xrandr --output HDMI-1 --primary
-xrandr --output eDP-1 --right-of HDMI-1 
-xrandr --output eDP-1 --brightness 0.85
-feh --bg-scale ~/Pictures/debian_wallpaper.jpg
+
 setxkbmap -option caps:escape
+#xrandr --output HDMI-1 --primary
+#xrandr --output eDP-1 --right-of HDMI-1 
+#xrandr --output eDP-1 --brightness 0.85
+
+xrandr --output eDP1 --mode 1366x768 --pos 1920x156 --rotate normal --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+
+feh --bg-scale ~/Pictures/debian_wallpaper.jpg
 #swap underscore with minus
 xmodmap -e "keycode 20 = underscore minus underscore minus"
 #to get back to normal
