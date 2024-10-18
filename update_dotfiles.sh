1302a3d996ce8bcef8f8b1710b5a61807633de44
Syed Rizwan <rizwansm@outlook.com>
2024-10-18 18:10:24 +0600

    minor change

---
+++ update_dotfiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <message>"
+    echo "Please state the changes."
+    exit 1
+fi
+
+message="$1"
+
+# Copy Hyprland configuration files
+cp ~/.config/hypr/* ~/dotfiles/hyprland/hyperland/
+
+# Copy Waybar configuration files
+cp ~/.config/waybar/* ~/dotfiles/hyprland/waybar/
+
+git add .
+
+git commit -m "$message"
+
+git push origin main
+
+echo "Configuration files have been updated."
