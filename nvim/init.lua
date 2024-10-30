293b8f4565e5a1f7b9967d8d955cd44a43d0f08b
Syed Rizwan <rizwansm@outlook.com>
2024-10-30 12:21:51 +0600

    added lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,16 @@
+require("config.lazy")
 vim.cmd([[
+    syntax on                   " syntax highlighting
     set nocompatible            " disable compatibility to old-time vi
     set tabstop=4               " number of columns occupied by a tab 
     set softtabstop=4           " see multiple spaces as tabstops so <BS> does the right thing
     set expandtab               " converts tabs to white space
     set shiftwidth=4            " width for autoindents
     set autoindent              " indent a new line the same amount as the line just typed
-    set number                  " add line numbers
+    set number relativenumber   " show actual and relative line number
     set wildmode=longest,list   " get bash-like tab completions
-    syntax on                   " syntax highlighting
     set mouse=a                 " enable mouse click
-"    set clipboard=unnamedplus   " using system clipboard
     set cursorline              " highlight current cursorline
     set ttyfast                 " Speed up scrolling in Vim
+    "set clipboard=unnamedplus   " using system clipboard
 ]])
