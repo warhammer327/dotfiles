20724f9c01b2bf87a15253f43b76f117c124e91a
Rizwan <rizwansm@outlook.com>
2025-07-27 09:00:41 +0600

    Add files via upload

---
+++ cmus/lib.pl
@@ -0,0 +1,77 @@
+/home/rizwan/Music/Agamoni.mp3
+/home/rizwan/Music/Desh Kar.mp3
+/home/rizwan/Music/Gandagana.mp3
+/home/rizwan/Music/Abdul Jabbar - Tumi Ki Dekhecho Kobhu.flac
+/home/rizwan/Music/Matir Roud.mp3
+/home/rizwan/Music/Arnob - Hok Kolorob.flac
+/home/rizwan/Music/Artcell - Dhushor Shomoy.flac
+/home/rizwan/Music/Artcell - Kandari Hushiar.flac
+/home/rizwan/Music/Avenged Sevenfold - Blinded in Chains.flac
+/home/rizwan/Music/Avenged Sevenfold - Hail to the King.flac
+/home/rizwan/Music/Black Sabbath - Paranoid.flac
+/home/rizwan/Music/Bon Jovi - It-_s My Life.flac
+/home/rizwan/Music/BAYANNO BANGLADESH.mp3
+/home/rizwan/Music/Disturbed - Down with the Sickness.flac
+/home/rizwan/Music/Disturbed - Indestructible.flac
+/home/rizwan/Music/Disturbed - The Vengeful One.flac
+/home/rizwan/Music/Disturbed - Tyrant.flac
+/home/rizwan/Music/DJ Shadow - Six Days.flac
+/home/rizwan/Music/Manush Manusher Janya.flac
+/home/rizwan/Music/Dr. Bhupen Hazarika - Ami Ek Jajabar.flac
+/home/rizwan/Music/Hey Dola Hey Dola.flac
+/home/rizwan/Music/Dreek - Jhornar Moto Chonchol.flac
+/home/rizwan/Music/Eluveitie - Isara.flac
+/home/rizwan/Music/Five Finger Death Punch - Wrong Side of Heaven.flac
+/home/rizwan/Music/Five Finger Death Punch - M.I.N.E (End This Way).flac
+/home/rizwan/Music/Five Finger Death Punch - Cold.flac
+/home/rizwan/Music/Five Finger Death Punch - Gone Away.flac
+/home/rizwan/Music/Dictionary by GaanPoka.mp3
+/home/rizwan/Music/Globus - Preliator.flac
+/home/rizwan/Music/Godsmack - Serenity.flac
+/home/rizwan/Music/Iron Maiden - For the Greater Good of God.flac
+/home/rizwan/Music/Iron Maiden - Dance Of Death.mp3
+/home/rizwan/Music/Iron Maiden - Hell On Earth.flac
+/home/rizwan/Music/Iron Maiden - Tears Of A Clown.flac
+/home/rizwan/Music/Iron Maiden - If Eternity Should Fail.flac
+/home/rizwan/Music/Iron Maiden - Senjutsu.flac
+/home/rizwan/Music/Death Of The Celts.flac
+/home/rizwan/Music/Iron Maiden - The Parchment.flac
+/home/rizwan/Music/John Williams - Williams The Tale Of Viktor Navorski.flac
+/home/rizwan/Music/Kaaktaal - Shikkhar Jobanbondi.mp3
+/home/rizwan/Music/Kaaktaal - Rokto Gorom Matha Thanda.mp3
+/home/rizwan/Music/Kaaktaal - Vorosha Ase.mp3
+/home/rizwan/Music/Kaaktaal - Golokdhadha.flac
+/home/rizwan/Music/KONGOS - Come with Me Now.flac
+/home/rizwan/Music/Lena Chamamyan - Lamma Bada Yatathana.flac
+/home/rizwan/Music/Linkin Park - Battle Symphony.mp3
+/home/rizwan/Music/Ludovico Einaudi - Einaudi Experience.flac
+/home/rizwan/Music/Ludwig van Beethoven - Für Elise.flac
+/home/rizwan/Music/Five Hundred Miles - Bullet Train (2022).m4a
+/home/rizwan/Music/Mcc-e Mac - INQUILAB.mp3
+/home/rizwan/Music/Meghdol - Ommm.flac
+/home/rizwan/Music/Metallica - Enter Sandman.mp3
+/home/rizwan/Music/Metallica - Fade To Black.mp3
+/home/rizwan/Music/Miracle Of Sound - Valhalla Calling.flac
+/home/rizwan/Music/Mohamed Youssef - Tala-_ Al Badru Alayna.flac
+/home/rizwan/Music/Nachiketa Chakraborty - Ambition.flac
+/home/rizwan/Music/Nirvana - Smells Like Teen Spirit.flac
+/home/rizwan/Music/You_re Dead.mp3
+/home/rizwan/Music/Mere Rashke Qamar.mp3
+/home/rizwan/Music/Ozzy Osbourne - Under the Graveyard.flac
+/home/rizwan/Music/Rotting Christ - In the Name of God.flac
+/home/rizwan/Music/Rotting Christ - Pir Threontai (Remix).flac
+/home/rizwan/Music/Rotting Christ - Devadevam.flac
+/home/rizwan/Music/Sabaton - Wehrmacht.flac
+/home/rizwan/Music/Sabaton - Ghost Division.flac
+/home/rizwan/Music/Sabaton - Panzerkampf.flac
+/home/rizwan/Music/Sabaton - Night Witches.flac
+/home/rizwan/Music/Sabaton - Bismarck.flac
+/home/rizwan/Music/Sabaton - Great War.flac
+/home/rizwan/Music/Sabaton - The Attack of the Dead Men.flac
+/home/rizwan/Music/Sabaton - Defence of Moscow.flac
+/home/rizwan/Music/Sabaton - Stormtroopers.flac
+/home/rizwan/Music/Shono Mohajon.mp3
+/home/rizwan/Music/Static-X - Skinnyman.flac
+/home/rizwan/Music/The Cranberries - Zombie.flac
+/home/rizwan/Music/Vikings Main Title - If I Had a Heart (-Vikings- Intro Song).flac
+/home/rizwan/Music/Woodkid - Run Boy Run.flac
