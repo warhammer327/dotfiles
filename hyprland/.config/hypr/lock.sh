8d7d2502cd3d90bd447ba18d6097026106bf44e4
Syed Rizwan <rizwansm@outlook.com>
2026-04-17 12:13:40 +0600

    first commit

--- hyprland/.config/hypr/lock.sh
+++ hyprland/.config/hypr/lock.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-monitors=("eDP-1" "HDMI-A-2")
-for monitor in "${monitors[@]}"; do
-  out="/tmp/lock-$monitor.png"
-  grim -o "$monitor" - |
-    magick - -scale 10% -scale 1000% "$out" &
-done
-wait
+grim -o eDP-1 /tmp/lock-eDP-1.png
+sleep 0.5
 hyprlock
